ce12115488350635362c987caf0010e75f212bc6
pvffyn <jottew@yahoo.com>
2021-06-03 21:25:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,9 @@ os.environ["JISHAKU_HIDE"] = "True"
 coloredlogs.install()
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename=f"data/logs/discord.log", encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 @tasks.loop(seconds=10)
 async def presence():
