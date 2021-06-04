dada365ec055e4848c8f11b422a86e1b074db08f
pvffyn <jottew@yahoo.com>
2021-06-04 15:47:52 +0200

    Fixed setprefix command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1127,9 +1127,9 @@ UUID: `{friend['uuid']}`
     @commands.cooldown(1,30,commands.BucketType.user)
     async def setprefix(self, ctx, prefix : str = "w,"):
         try:
-            await self.bot.db.execute("INSERT INTO prefixes (guild, prefix) VALUES ($1, $2)", ctx.guild.id, prefix)
+            await self.bot.db.execute("INSERT INTO prefixes (user_id, prefix) VALUES ($1, $2)", ctx.author.id, prefix)
         except asyncpg.UniqueViolationError:
-            await self.bot.db.execute("UPDATE prefixes SET prefix = $1 WHERE guild = $2", prefix, ctx.guild.id)
+            await self.bot.db.execute("UPDATE prefixes SET prefix = $1 WHERE user_id = $2", prefix, ctx.author.id)
         em=discord.Embed(description=f"Sucessfully set your prefix to `{prefix}`", color=color())
         em.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.reply(embed=em, mention_author=False)
